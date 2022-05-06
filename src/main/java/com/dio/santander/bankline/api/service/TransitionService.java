ff9f9b41f704d644e243a35417e463bf221e3c1d
Leonardo Vogel <leeo.vog13@gmail.com>
2022-05-06 04:48:03 -0300

    :sparkles: feat: add functionality to change client balance when add a new transition

--- src/main/java/com/dio/santander/bankline/api/service/TransitionService.java
+++ src/main/java/com/dio/santander/bankline/api/service/TransitionService.java
@@ -6,14 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dio.santander.bankline.api.dto.NewTransition;
+import com.dio.santander.bankline.api.model.Client;
 import com.dio.santander.bankline.api.model.Transition;
 import com.dio.santander.bankline.api.model.TransitionType;
+import com.dio.santander.bankline.api.repository.ClientRepository;
 import com.dio.santander.bankline.api.repository.TransitionRepository;
 
 @Service
 public class TransitionService {
 	@Autowired
 	private TransitionRepository repository;
+	
+	@Autowired
+	private ClientRepository clientRepository;
+	
 	public void save(NewTransition newTransition) {
 		Transition transition = new Transition();
 		
@@ -26,6 +32,12 @@ public class TransitionService {
 		transition.setAccountId(newTransition.getAccountId());
 		transition.setType(newTransition.getType());
 		transition.setValue(value);
+		
+		Client client = clientRepository.findById(newTransition.getAccountId()).orElse(null);
+		if (client != null) {
+			client.getAccount().setBalance(client.getAccount().getBalance() + value);
+			clientRepository.save(client);
+		}
 		
 		repository.save(transition);
 	}
