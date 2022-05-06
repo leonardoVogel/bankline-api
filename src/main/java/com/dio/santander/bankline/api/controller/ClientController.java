5f7c5b1c17d61f9e7c09f32128b5094ad192c5bc
Leonardo Vogel <leeo.vog13@gmail.com>
2022-05-06 04:09:02 -0300

    :sparkles: feat: create operation to add a new client to database

--- src/main/java/com/dio/santander/bankline/api/controller/ClientController.java
+++ src/main/java/com/dio/santander/bankline/api/controller/ClientController.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dio.santander.bankline.api.dto.NewClient;
 import com.dio.santander.bankline.api.model.Client;
 import com.dio.santander.bankline.api.repository.ClientRepository;
+import com.dio.santander.bankline.api.service.ClientService;
 
 @RestController
 @RequestMapping("/clients")
@@ -16,9 +20,16 @@ public class ClientController {
 	@Autowired
 	private ClientRepository repository;
 	
+	@Autowired
+	private ClientService service;
+	
 	@GetMapping
 	public List<Client> findAll() {
 		return repository.findAll();
 	}
 
+	@PostMapping
+	public void save(@RequestBody NewClient client) {
+		service.save(client);
+	}
 }
---
+++ src/main/java/com/dio/santander/bankline/api/service/ClientService.java
@@ -0,0 +1,30 @@
+package com.dio.santander.bankline.api.service;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dio.santander.bankline.api.dto.NewClient;
+import com.dio.santander.bankline.api.model.Account;
+import com.dio.santander.bankline.api.model.Client;
+import com.dio.santander.bankline.api.repository.ClientRepository;
+
+@Service
+public class ClientService {
+	@Autowired
+	private ClientRepository repository;
+	public void save(NewClient newClient) {
+		Client client = new Client();
+		
+		client.setCpf(newClient.getCpf());
+		client.setNome(newClient.getName());
+		
+		Account account = new Account();
+		account.setBalance(0.0);
+		account.setNumber(new Date().getTime());
+		
+		client.setAccount(account);
+		repository.save(client);
+	}
+}
